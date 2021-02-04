3b6bc85ac0f803c14e6cf4d695c4830092699d8c
Eduardo Mitkus <eduardo.mitkus@gmail.com>
2021-02-04 03:00:35 -0300

    Adiciona verificação de existência de registros no banco, caso haja, o fluxo encerrará

--- app/controllers/task/starship_controller.rb
+++ app/controllers/task/starship_controller.rb
@@ -4,8 +4,13 @@ require_relative '../../services/starwars_service'
 class StarshipController < ApplicationController
 
   def self.create
+    if Starship.all.exists?
+      return "As naves já foram salvas no database"
+    end
+
     starshipsResponse = StarwarsService.getStarships
     Starship.create(starshipsResponse)
+    return "Naves salvas no database"
   end
 
 end
