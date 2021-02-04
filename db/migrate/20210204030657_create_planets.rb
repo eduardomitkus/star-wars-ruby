4b390e4b703c0b1d746130587098895606788471
Eduardo Mitkus <eduardo.mitkus@gmail.com>
2021-02-04 00:08:54 -0300

    Adiciona Model para Planetas

---
+++ db/migrate/20210204030657_create_planets.rb
@@ -0,0 +1,19 @@
+class CreatePlanets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :planets do |t|
+      t.string :name
+      t.string :climate
+      t.integer :diamater
+      t.string :gravity
+      t.integer :orbital_period
+      t.integer :population
+      t.string :terrain
+      t.references :homeworld, index:true, foreign_key: {to_table: :breeds}
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :planets
+  end
+end
