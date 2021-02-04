b6ed6712628f2e30aa5db8f66240306a01fc03f8
Eduardo Mitkus <eduardo.mitkus@gmail.com>
2021-02-04 00:11:33 -0300

    Comita Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_02_04_030929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "breeds", force: :cascade do |t|
+    t.string "name"
+    t.string "clasification"
+    t.string "designation"
+    t.string "language"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "birth_year"
+    t.string "eye_color"
+    t.string "gender"
+    t.string "hair_color"
+    t.string "skin_color"
+    t.string "mass"
+    t.string "height"
+    t.bigint "planet_id"
+    t.bigint "starship_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["planet_id"], name: "index_people_on_planet_id"
+    t.index ["starship_id"], name: "index_people_on_starship_id"
+  end
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name"
+    t.string "climate"
+    t.integer "diamater"
+    t.string "gravity"
+    t.integer "orbital_period"
+    t.integer "population"
+    t.string "terrain"
+    t.bigint "homeworld_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["homeworld_id"], name: "index_planets_on_homeworld_id"
+  end
+
+  create_table "starships", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.string "manufacturer"
+    t.string "max_atmosphering_speed"
+    t.integer "passengers"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "people", "planets"
+  add_foreign_key "people", "starships"
+  add_foreign_key "planets", "breeds", column: "homeworld_id"
 end
